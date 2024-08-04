89d0aadf721e5e38629fd5e0ecab5858505da958
Kovalenko Dmytro <kovalenko.dm80@gmail.com>
2024-08-04 13:57:27 +0300

    [*] Pipeline started

--- src/test/java/com/gmail/apach/jenkins_demo/AbstractControllerIntegrationTest.java
+++ src/test/java/com/gmail/apach/jenkins_demo/AbstractControllerIntegrationTest.java
@@ -1,15 +1,10 @@
 package com.gmail.apach.jenkins_demo;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.junit.jupiter.api.BeforeEach;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
-
-import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 
 @SpringBootTest(
 	classes = JenkinsDemoApplication.class,
@@ -21,14 +16,4 @@ public abstract class AbstractControllerIntegrationTest extends AbstractIntegrat
 	protected MockMvc mvc;
 	@Autowired
 	protected ObjectMapper objectMapper;
-	@Autowired
-	private WebApplicationContext context;
-
-	@BeforeEach
-	public void setup() {
-		mvc = MockMvcBuilders
-			.webAppContextSetup(context)
-			.apply(springSecurity())
-			.build();
-	}
 }
